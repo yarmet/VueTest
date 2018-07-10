b9da780d83ddebcc978c6453116e655f3fb3ea08
ruslan <yarmet84@mail.ru>
2018-07-11 01:33:15 +0300

    добавил новый компонент Selector

---
+++ src/components/linkcomponents/Selector.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <div>
+    <select class="custom-select-sm" v-model="items.selected">
+      <option v-for="item in items.availableGroups" v-bind:key="item" v-bind:value="item">{{item[label]}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Selector',
+  props: ['items', 'label']
+}
+
+</script>
+
+<style scoped>
+
+</style>
